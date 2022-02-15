99f5e5903f80dce23ea6d643a103c600d97cbcec
Haibin Chu <97762703+rdcchhb@users.noreply.github.com>
2022-02-14 23:52:54 -0500

    Create hello.py

---
+++ scripts/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A simple Python script """
+
+
+def main():
+
+    # this will be displayed to standard out
+    print("hello from Haibin")
+    
+if __name__ == "__main__":
+    main()
